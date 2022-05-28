3d967ccef5041c64ae325e7e49d0c99220afb96e
Vit <s1te@bk.ru>
2022-05-28 18:37:05 +0300

    work2508

--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'CourseController@index');
+Route::get('/', function(){
+   return view('welcome');
+});
+
+Route::get('/111', 'CourseController@index');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::group(['middleware' => ['role:admin']], function () {
+   Route::get('/control-panel', function(){
+      echo 'q';
+   });
+});
